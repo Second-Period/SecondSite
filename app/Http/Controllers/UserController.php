6e2821c752f14b7979d701ab9884bf44d60fd4b3
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-09-11 11:51:23 -0300

    Add files via upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    function index() {
+        return view('admin.users', ['users' => $this->getUsers()]);  
+    }
+    function getUsers() {
+        $users  = User::all();
+        return $users;
+    }
+
+}
