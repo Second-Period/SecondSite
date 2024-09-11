ef9a1189e69a2eb56b963e8200c1b228234e251d
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-09-11 13:46:28 -0300

    Add files via upload

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    //
+    public function index(){
+        $products = Products::all();
+        return view('admin.products.list_products',['products' => $products]);
+    }
+    public function renderiza_tela(){
+        return view('admin.products.create_product');
+    }
+    public function create(Request $request){
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric'],
+            'product_img' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+        ]);
+            
+        $file = rand(0,9999).'-'.$request->file('product_img')->getClientOriginalName();
+        $path = $request->file('product_img')->storeAs('upload', $file);
+            
+        $product = Products::create([
+            'name' => $request->name,
+            'price' => $request->price,
+            'product_img' => $path
+        ]);
+            
+        return redirect()->route('products.index')->with('success', 'Produto salvo com sucesso!'); 
+    }
+
+    public function detail($id){
+        $product = Products::find($id);
+        return view('admin.products.detail_product',['product' => $product]);        
+    }
+    
+    public function update(Request $request, $id){
+        $product = Products::find($id);
+        $product->update($request->all());
+        return redirect()->route('products.index')->with('success', 'Produto atualizado com sucesso!');  
+    }
+
+    public function delete($id){
+        $product = Products::find($id);
+        $product->delete();
+        return redirect()->route('products.index')->with('success', 'Produto excluido com sucesso!');  
+    }
+    
+}
--- app/Models/Products.php
+++ app/Models/Products.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'price',
+        'product_img',
+    ];
+
+    protected $casts = [
+        'price' => 'float',
+    ];
 }
