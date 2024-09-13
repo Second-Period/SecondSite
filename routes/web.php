35491b872e40a27af3a988816ca5b3be8136d029
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-09-13 13:07:09 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,13 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/', function () {
+    return view('index');
+})->name('page_home');
 
 Route::middleware(['auth', 'verified',AdminMiddleware::class])->group(function () {
     Route::group(['prefix' => 'admin'], function () {
