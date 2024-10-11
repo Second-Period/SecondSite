1d08f11003542c1fecdec902ffa2a59559173776
Dante-Lucas <lucassilva270805@gmail.com>
2024-10-11 13:59:26 -0300

    Adicionando comentários no arquivo artisan.bat

--- .bat/artisan.bat
+++ .bat/artisan.bat
@@ -1,4 +1,6 @@
-REM SCRIPT PARA RODAR QUALQUER COMANDO LISTADO PELO ARTISAN. ADICIONE UM ARGUMENTO DEPOIS DE CHAMAR O ARQUIVO .BAT
+ ::SCRIPT PARA RODAR QUALQUER COMANDO LISTADO PELO ARTISAN. 
+ ::ADICIONE UM ARGUMENTO DEPOIS DE CHAMAR O ARQUIVO .BAT
+ :: artisan serve onde serve é um dos argumentos que o artisan aceita ou migrate ou qualquer outro do laravel
 @echo off
 call php artisan %*
 pause
